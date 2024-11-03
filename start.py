5ed293a2d9f5d0a440cb93aef001ca0cbff79cc3
KraciukPiotr <biz.piotrkraciuk@gmail.com>
2024-11-03 18:55:07 +0100

    Nowe pliki z pierwszej lekcji

---
+++ start.py
@@ -0,0 +1,21 @@
+
+# 1. Stwórz zmienną `a` o wartości 15 i zmienną `b` o wartości 5. Wypisz ich sumę.
+
+a, b = 15, 5
+print(a + b)
+
+# 2. Zdefiniuj zmienną `x` jako liczbę zmiennoprzecinkową (np. 10.5). Pomnóż ją przez 3 i wypisz wynik.
+
+x = 10.5
+print(x * 3)
+
+# 16. Przypisz do zmiennej `a` wartość 10, a do zmiennej `b` wartość 20. Następnie zamień wartości zmiennych miejscami. **Hint:** Nie było ale pogłówkujcie ;) 
+
+a, b = 10, 20
+
+a, b = b, a 
+print(a, b)
+
+# 18. Utwórz zmienną `wiek` i przypisz do niej wartość liczbową. Następnie wypisz zdanie: "Mam X lat", gdzie X to wartość zmiennej `wiek`. **Hint:** Użyj f-stringa, aby w łatwy sposób wstawić wartość zmiennej do tekstu, np. `print(f"{zmienna}")`.
+wiek = 20
+print(f"Mam {wiek} lat")
